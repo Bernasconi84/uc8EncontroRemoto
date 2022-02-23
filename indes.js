7398358d4d4e61e57d670f3f136ca2dcd2022e64
bernasconi <bernasconi84@outlook.com>
2022-02-22 22:50:19 -0300

    Código do desafio solicitado no Encontro Remoto UC8 Senai

---
+++ indes.js
@@ -0,0 +1,20 @@
+var numTotalAlunos = 24;
+var contador = 0;
+var numero;
+
+while (contador < numTotalAlunos) {
+    console.log(`Alunos ${contador}`)
+contador++;    
+}
+    if (numTotalAlunos === 0) {
+        return console.log("O número é zero");
+    } else if (numTotalAlunos !== 0) {
+        numero = numTotalAlunos % 2
+    } if (numero === 0) {
+        console.log(`Temos ${numTotalAlunos} alunos, número par! `);
+    } else {
+        console.log(`Temos ${numTotalAlunos} alunos, número ímpar! `)
+
+    }
+
+
